3863ddc2a49ba3b4e62711c207c9b2acbae553d1
whyntter <67507345+whyntter@users.noreply.github.com>
2020-06-27 18:07:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,40 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+const fs = require('fs');
+let config = require('./botconfig.json');
+let token = config.token;
+let prefix = config.prefix;
+
+fs.readdir('./cmds/', (err, files) => {
+    if (err) console.log(err);
+    let jsfiles = files.filter(f => f.split(".").pop() === "js");
+    if (jsfiles.length <= 0) console.log("no commands to load!!");
+    console.log(`Loaded ${jsfiles.length} command(s)`);
+    jsfiles.forEach((f, i) => {
+        let props = require(`./cmds/${f}`);
+        console.log(`${i+1}.${f} Loaded!`);
+        client.commands.set(props.help.name, props);
+    });
+});
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+client.login(token);
+
+client.on('message', async message => {
+    if (message.author.bot) return; 
+    if (message.chanel.type == "dm") return; 
+    let user = message.author.username;
+    let userid = message.author.id;
+    let messageArray = message.content.split(" "); 
+    let command = messageArray[0].toLowerCase(); 
+    let args = messageArray.slice(prefix.lenght); 
+    if (!message.content.startsWith(prefix)) return; 
+    let cmd = client.commands.get(command.slice(prefix.length));
+    if (cmd) cmd.run(client, message, args);
 });
 
-// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
